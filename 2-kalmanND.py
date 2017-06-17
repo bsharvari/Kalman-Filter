ad43afae981666e36ccf6015fa9920bdb8984672
Sharvari Bhatwadekar <sharvariab@gmail.com>
2017-06-17 00:06:41 -0600

    Add files via upload

---
+++ 2-kalmanND.py
@@ -0,0 +1,176 @@
+# A generalized multi-dimensional Kalman Filter implementation
+
+from math import *
+
+
+class matrix:
+    # Implements basic operations of a matrix class
+
+    def __init__(self, value):
+        self.value = value
+        self.dimx = len(value)
+        self.dimy = len(value[0])
+        if value == [[]]:
+            self.dimx = 0
+
+    def zero(self, dimx, dimy):
+        # check if dimensions are valid
+        if dimx < 1 or dimy < 1:
+            raise ValueError("Invalid size of matrix")
+        else:
+            self.dimx = dimx
+            self.dimy = dimy
+            self.value = [[0 for row in range(dimy)] for col in range(dimx)]
+
+    def identity(self, dim):
+        # Check if dimensions are valid
+        if dim < 1:
+            raise ValueError("Invalid size of matrix")
+        else:
+            self.dimx = dim
+            self.dimy = dim
+            self.value = [[0 for row in range(dim)] for col in range(dim)]
+            for i in range(dim):
+                self.value[i][i] = 1
+
+    def show(self):
+        for i in range(self.dimx):
+            print self.value[i]
+        print ' '
+
+    def __add__(self, other):
+        # Check if dimensions are valid
+        if self.dimx != other.dimx or self.dimy != other.dimy:
+            raise ValueError("Matrices must be of equal dimensions to add")
+        else:
+            # Add two matrices, if correct dimensions
+            res = matrix([[]])
+            res.zero(self.dimx, self.dimy)
+            for i in range(self.dimx):
+                for j in range(self.dimy):
+                    res.value[i][j] = self.value[i][j] + other.value[i][j]
+            return res
+
+    def __sub__(self, other):
+        # Check if dimensions are valid
+        if self.dimx != other.dimx or self.dimy != other.dimy:
+            raise ValueError("Matrices must be of equal dimensions to subtract")
+        else:
+            # Subtract two matrices, if correct dimensions
+            res = matrix([[]])
+            res.zero(self.dimx, self.dimy)
+            for i in range(self.dimx):
+                for j in range(self.dimy):
+                    res.value[i][j] = self.value[i][j] - other.value[i][j]
+            return res
+
+    def __mul__(self, other):
+        # Check if dimensions are valid
+        if self.dimy != other.dimx:
+            raise ValueError("Matrices must be m*n and n*p to multiply")
+        else:
+            # Multiply two matrices, if correct dimensions
+            res = matrix([[]])
+            res.zero(self.dimx, other.dimy)
+            for i in range(self.dimx):
+                for j in range(other.dimy):
+                    for k in range(self.dimy):
+                        res.value[i][j] += self.value[i][k] * other.value[k][j]
+            return res
+
+    def transpose(self):
+        # Compute transpose
+        res = matrix([[]])
+        res.zero(self.dimy, self.dimx)
+        for i in range(self.dimx):
+            for j in range(self.dimy):
+                res.value[j][i] = self.value[i][j]
+        return res
+
+    # Thanks to Ernesto P. Adorio for use of Cholesky and Cholesky Inverse functions
+
+    def Cholesky(self, ztol=1.0e-5):
+        # Computes the upper triangular Cholesky factorization of a positive definite matrix.
+        res = matrix([[]])
+        res.zero(self.dimx, self.dimx)
+
+        for i in range(self.dimx):
+            S = sum([(res.value[k][i]) ** 2 for k in range(i)])
+            d = self.value[i][i] - S
+            if abs(d) < ztol:
+                res.value[i][i] = 0.0
+            else:
+                if d < 0.0:
+                    raise ValueError, "Matrix not positive-definite"
+                res.value[i][i] = sqrt(d)
+            for j in range(i + 1, self.dimx):
+                S = sum([res.value[k][i] * res.value[k][j] for k in range(self.dimx)])
+                if abs(S) < ztol:
+                    S = 0.0
+                res.value[i][j] = (self.value[i][j] - S) / res.value[i][i]
+        return res
+
+    def CholeskyInverse(self):
+        # Computes inverse of matrix given its Cholesky upper Triangular decomposition of matrix.
+        res = matrix([[]])
+        res.zero(self.dimx, self.dimx)
+
+        # Backward step for inverse.
+        for j in reversed(range(self.dimx)):
+            tjj = self.value[j][j]
+            S = sum([self.value[j][k] * res.value[j][k] for k in range(j + 1, self.dimx)])
+            res.value[j][j] = 1.0 / tjj ** 2 - S / tjj
+            for i in reversed(range(j)):
+                res.value[j][i] = res.value[i][j] = -sum(
+                    [self.value[i][k] * res.value[k][j] for k in range(i + 1, self.dimx)]) / self.value[i][i]
+        return res
+
+    def inverse(self):
+        aux = self.Cholesky()
+        res = aux.CholeskyInverse()
+        return res
+
+    def __repr__(self):
+        return repr(self.value)
+
+
+########################################
+
+def kalman_filter(x, P):
+    for n in range(len(measurements)):
+
+        # Prediction
+        x = (F * x) + u
+        P = F * P * F.transpose()
+
+        # Measurement update
+        z = matrix([[measurements[n]]])
+        y = z.transpose() - (H * x)
+        S = (H * P * H.transpose()) + R
+        K = P * H.transpose() * S.inverse()
+
+        x = x + (K * y)
+        P = (I - (K * H)) * P
+
+    return x, P
+
+
+measurements = [[5., 10.], [6., 8.], [7., 6.], [8., 4.], [9., 2.], [10., 0.]]
+initial_xy = [4., 12.]
+
+dt = 0.1
+
+x = matrix([[initial_xy[0]], [initial_xy[1]], [0.], [0.]])  # initial state (location and velocity)
+u = matrix([[0.], [0.], [0.], [0.]])  # external motion
+
+P = matrix([[0., 0., 0., 0.], [0., 0., 0., 0.], [0., 0., 1000., 0.],
+            [0., 0., 0., 1000.]])  # initial uncertainty: 0 for positions x and y, 1000 for the two velocities
+F = matrix([[1., 0., dt, 0.], [0., 1., 0., dt], [0., 0., 1., 0.],
+            [0., 0., 0., 1.]])  # next state function: generalize the 2d version to 4d (x + dt*xdot)
+H = matrix(
+    [[1., 1., 0., 0.]])  # measurement function: reflect the fact that we observe x and y but not the two velocities
+R = matrix([[dt, 0.], [0., dt]])  # measurement uncertainty: use 2x2 matrix with 0.1 as main diagonal
+I = matrix([[1., 0., 0., 0.], [0., 1., 0., 0.], [0., 0., 1., 0.], [0., 0., 0., 1.]])  # 4d identity matrix
+
+print kalman_filter(x, P)
+
